36a31c9e9940ba31f183ecb45ca66d19fa0eabd5
Ben Medeiros <benmedeiros3@gmail.com>
2024-12-20 20:51:33 -0500

    Add Dockerfile and scripts for container management
    - Created Dockerfile for application setup
    - Added docker_info_scripts.bat for container operations
    
    Total files committed: 2

---
+++ docker_info_scripts.bat
@@ -0,0 +1,14 @@
+rem Misc scripts for Docker
+
+
+
+docker container ls
+rem start the mysql container
+docker container start 1b7f4aed158f
+
+
+rem monitor activity across all instances 
+docker events
+
+rem build container using dockerfile
+docker build -t my-image -f ./dockerfiles/MyDockerfile .
